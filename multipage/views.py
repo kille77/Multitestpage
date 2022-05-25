5dfaf6d3103b20b4b007b74c3ed9c485e2296535
kille77 <teemu-97@live.fi>
2022-05-25 19:05:14 +0300

    Committed changes

--- multipage/views.py
+++ multipage/views.py
@@ -20,6 +20,7 @@ def titanic(request):
     return render(request,'titanic.html')
 
 def result(request):
+    name = str(request.GET["name"])
     pclass = int(request.GET["pclass"])
     sex = int(request.GET["sex"])
     age = int(request.GET["age"])
@@ -29,4 +30,39 @@ def result(request):
     embarked = int(request.GET["embarked"])
     title = int(request.GET["title"])
     prediction = Prediction_model.prediction_model(pclass,sex,age,sibsp,parch,fare,embarked,title)
-    return render(request, 'result.html', {'prediction':prediction})
+
+    if prediction == 0:
+        prediction = "You did not survive titanic voyage :("
+    elif prediction == 1:
+        prediction = "Yay, you survived titanic!"
+
+    if sex == 0:
+        sex = "male"
+    elif sex == 1:
+        sex = "female"
+
+    if embarked == 0:
+        embarked = "Southampton"
+    elif embarked == 1:
+        embarked = "Cherbourg"
+    elif embarked == 2:
+        embarked = "Queenstown"
+
+
+    if title == 0:
+        title = "Mr"
+    elif title == 1:
+        title = "Miss"
+    elif title == 2:
+        title = "Mrs"
+    elif title == 3:
+        title = "Master"
+    elif title == 4:
+        title = "Dr"
+    elif title == 5:
+        title = "Rev"
+    elif title == 6:
+        title = "Officer"
+    elif title == 7:
+        title = "Royalty"
+    return render(request, 'result.html', {'prediction':prediction, 'pclass':pclass, 'sex':sex, 'age':age, 'sibsp':sibsp, 'parch':parch, 'fare':fare, 'embarked':embarked, 'title':title, 'name':name})
