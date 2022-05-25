2d9d2cfa25137c4f8d6525bcfffed4f5d9694772
kille77 <98214249+kille77@users.noreply.github.com>
2022-05-25 19:13:23 +0300

    Update settings.py
    deleted key

--- multipage/settings.py
+++ multipage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kcd1s1^#@fok#d0rhlji5u#kd+ikb=)8v_!k8&y1_3+=*tvud6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
